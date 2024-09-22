7f1a325484f54fb07371e19289df7b9110edc168
siddhant29 <siddhant.bishnoi@janio.asia>
2024-09-23 00:44:49 +0530

    principal_type = '' fixed read-only condition

--- role-vending-machine/role-new.tf
+++ role-vending-machine/role-new.tf
@@ -9,9 +9,10 @@ module "example_security_inf_repo_Production" {
 
   # Trusted GitHub repository
   repository_name = "bishnoi-sid"
-
+  role_name = "test-siddhant-rvm"
   # Trusted branch for write operations
   github_branch = "main"
+  principal_type  = ""
 
   # Specify the least permissions required for this pipeline to run
   inline_policy = data.aws_iam_policy_document.example_security_inf_repo_Production_permissions.json
