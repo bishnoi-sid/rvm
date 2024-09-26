8ca8609ecbcae7ff6a4c50379ef0034fd0dfe7c7
siddhant29 <siddhant.bishnoi@janio.asia>
2024-09-26 19:47:08 +0530

    testing policy

--- role-vending-machine/role-new.tf
+++ role-vending-machine/role-new.tf
@@ -25,19 +25,6 @@ module "example_security_inf_repo_Production" {
 data "aws_iam_policy_document" "example_security_inf_repo_Production_permissions" {
   # Can include multiple statements
   statement {
-    sid    = "SQSQueues"
-    effect = "Allow"
-    actions = [
-      "sqs:CreateQueue",
-      "sqs:DeleteQueue",
-      "sqs:TagQueue",
-      "sqs:UntagQueue",
-      "sqs:SetQueueAttributes"
-    ]
-    # Reference account numbers using the `variables-accounts` file
-    resources = ["arn:aws:sqs:*:${var.account_Production}:aws-s3-access-logs"]
-  }
-  statement {
     sid = "1"
     actions = [
       "s3:ListAllMyBuckets",
